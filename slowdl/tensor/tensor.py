c1f632d722e1928d51de89d9db8cee3c12695026
danielepaliotta <daniele.paliotta@truelayer.com>
2019-09-19 10:13:34 +0100

    Tensors

--- slowdl/tensor/tensor.py
+++ slowdl/tensor/tensor.py
@@ -2,5 +2,6 @@ import numpy as np
 
 
 class Tensor(object):
-    def __init__(self, data):
+    def __init__(self, data, grad=True):
         self.data = data
+        self.grad = grad
--- tests/test_tensor.py
+++ tests/test_tensor.py
@@ -1,9 +1,12 @@
 import pytest
+import numpy as np
 from slowdl.tensor import Tensor
 
 
 @pytest.mark.unittest
 def test_create_tensor():
+    data = np.zeros([5, 5])
+    tensor = Tensor(data)
 
-    tensor = Tensor([])
     assert type(tensor) == Tensor
+    assert (tensor.data == data).all()
